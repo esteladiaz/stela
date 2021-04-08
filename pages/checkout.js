d2a7f0b6dc7970029b3b70ea7543651f29ac6a16
esteladiaz <diazsanchez.estela@gmail.com>
2021-04-08 16:33:49 -0700

    fix

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
-import Nav from "../components/nav";
 
 const Checkout = () => (
   <div>
     <Head>
       <title>Checkout</title>
     </Head>
-
-    <Nav />
 
     <h1>Checkout</h1>
     <p>Please click the button below to complete your order</p>
